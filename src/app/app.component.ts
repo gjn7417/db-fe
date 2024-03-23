4c419516bf86c1ecff1c557ba342c44fa224238e
Gabe Naples <gjn7417@nyu.edu>
2024-03-22 23:43:28 -0400

    add project skeleton, navigation, basic tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { NavComponent } from './nav/nav.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, SidenavComponent, NavComponent]
 })
 export class AppComponent {
   title = 'databases-class';
