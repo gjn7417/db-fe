1cc8a2802ad7a0377ec8a549c2a513221ce94ded
Gabe Naples <gjn7417@nyu.edu>
2024-03-22 23:50:15 -0400

    add project skeleton, navigation, basic tables

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {ExpandableTableComponent} from "../expandable-table/expandable-table.component";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    ExpandableTableComponent,
+    MatGridList,
+    MatGridTile,
+    MatCardContent,
+    MatCard,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardTitle
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.http.get('').subscribe(data => {
+      console.log(data);
+    });
+  }
+
+}
